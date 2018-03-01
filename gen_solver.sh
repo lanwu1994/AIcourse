02a27248c015f7e3687edd4deb6aaf7bdd88babb
LANWU <lanwu1994@gmail.com>
2018-03-01 21:09:18 +0200

    d

--- gen_solver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "$#" -ne 1 ]; then
-    echo "Usage: `basename $0` <4|9>"
-    exit
-fi
-n=$1
-./gen_solver.py $n > tmp/solver$n.fml
-./ground.sh $n tmp/solver$n.fml > tmp/solver$n.sm
